bf344064d2729178490d2114a313e3489029fb11
AbubakirovRA <AbubakirovRA@ya.ru>
2021-11-01 03:23:04 +0500

    add methods in dev1

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,2 +1,61 @@
-﻿// Таким образом, а именно после двух слэшей, в C# пишутся комментарии
-Console.WriteLine(value: "2 Hello, World!");
+﻿// The task about two friends and their dog from lesson #1 of course "Intro in Programming"
+Console.Clear();
+
+Console.WriteLine("Условие задачи:");
+Console.WriteLine("Два друга находятся на каком-то расстоянии друг от друга и идут навстречу по дороге");
+Console.WriteLine("В момент начала движения, от первого друга ко второму выбегает собака,");
+Console.WriteLine("Достигнув второго друга собака разворачивается и бежит обратно, к первому другу");
+Console.WriteLine("Собака бегает между ними до тех пор, пока они не встретятся.");
+Console.WriteLine("Требуется вычислить сколько раз собака сбегает от одного друга к другому до момента их встречи.");
+
+Console.Write("Готовы вводить данные? (y/n)");
+
+string condition = Console.ReadLine();
+
+if (condition.ToLower() == "y")
+{
+    Console.WriteLine("Здорово! Приступим.");
+
+    Console.WriteLine("Введите скорость движения первого друга (м/сек)");
+    double first_friend_speed = Convert.ToDouble (Console.ReadLine());
+
+    Console.WriteLine("Введите скорость движения второго друга (м/сек)");
+    double second_friend_speed = Convert.ToDouble (Console.ReadLine());
+
+    Console.WriteLine("Введите скорость движения их собаки (м/сек)");
+    double dog_speed = Convert.ToDouble (Console.ReadLine());
+
+    Console.WriteLine("Введите расстояние между друзьями в начале движения (м)");
+    double distance = Convert.ToDouble (Console.ReadLine());
+
+    Console.WriteLine("Введите расстояние между друзьями при их встрече (м)");
+    double distance_min = Convert.ToDouble (Console.ReadLine());
+
+double overall_speed_dog_first_friend = first_friend_speed + dog_speed;
+
+double overall_speed_dog_second_friend = second_friend_speed + dog_speed;
+
+double count = 0;
+
+while (distance > distance_min)
+{
+    if (count % 2 != 0)
+    {
+        distance = distance - (first_friend_speed + second_friend_speed) * (distance / overall_speed_dog_first_friend);
+    }
+
+    else
+    {
+        distance = distance - (first_friend_speed + second_friend_speed) * (distance / overall_speed_dog_second_friend);
+    }
+
+    count++;
+
+}
+Console.WriteLine($"Ответ: собака сбегала от одного друга ко второму  {count} раз");
+}
+
+else
+{
+    Console.WriteLine("Завершаю работу.");
+}
--- dev1/Program.cs
+++ dev1/Program.cs
@@ -1,57 +1,147 @@
-﻿// Cформировать случайным образом целочисленный массив A из натуральных двузначных чисел. Создать на его основе масив B, отбрасывая те, которые
-
-//     нарушают порядок возрастания
-//     больше среднего арифметического элементов A
-//     чётные
-
-// Задача
-// Cформировать случайным образом целочисленный массив A
-// из натуральных двузначных чисел.
-
-// Создать на его основе масив B, отбрасывая те, которые
-// 1  нарушают порядок возрастания
-// 2  больше среднего арифметического элементов A
-// 3  чётные
- 
-
-// Задать массив
-
-
-
-int index = 0;
-
-int[] array = new int[10];
-// Заполняет массив
-while (index < 10)
+﻿int RightOrderSize(int[] collection) // Исключение элементов нарушения возрастания
 {
-    array[index] = new Random().Next(10, 100); // [10, 99]
-    index++;
-}
+    int length = collection.Length;
+    int index = 1;
+    int currentElement = collection[0];
+    int size = 0;
 
-index = 0;
-// печатаем массив
-
-while (index <= 9)
-{
-    int val = array[index];
-    Console.WriteLine(val);
-    index++;
-}
-
-Console.WriteLine();
-index = 1;
-// Исключение элементов нарушения возрастания
-int currentElement = array[0];
-Console.WriteLine(currentElement);
-while (index <= 9)
-{
-    if(array[index]>currentElement)
+    while (index < length)
     {
-        Console.WriteLine(array[index]);
-        currentElement = array[index];
+        if (collection[index] > currentElement)
+        {
+            currentElement = collection[index];
+            size = size + 1;
+        }
+        index++;
     }
-    index++;
+
+    return size;
+
 }
+
+void RightOrderFill(int[] collection, int[] size)
+{
+    int length = collection.Length;
+    int index = 1;
+    int indexB = 1;
+    int currentElement = collection[0];
+    size[0] = collection[0];
+
+    while (index < length)
+    {
+        if (collection[index] > currentElement)
+        {
+            currentElement = collection[index];
+            size[indexB] = collection[index];
+            indexB++;
+        }
+        index++;
+    }
+
+}
+
+void FillArray(int[] collection) // Объявляем метод FillArray, который будет заполнять массив 
+{
+    int length = collection.Length; //присваиваем переменной length значение, соответствующее количеству
+                                    //элементов в аргументе collection
+    int index = 0;                  //устанавливаем счетчик index в нуль
+    while (index < length)          //запускаем заполнение объявляя цикл до значения переменной length
+    {                                                           // тело цикла
+        collection[index] = new Random().Next(1, 10);       //заполняем аргумент collection случайными числами
+        //index = index + 1;                                //увеличиваем счетчик на 1
+        index++;
+    }                                                           //тело цикла
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+    Console.WriteLine();
+}
+
+void PrintTask(string[] text) // Объявляем метод PrintTask   
+{
+    int length = text.Length; //присваиваем переменной length значение, соответствующее количеству
+                              //элементов в аргументе text
+    int index = 0;                  //устанавливаем счетчик index в нуль
+    Console.Clear();
+    while (index < length)          //запускаем заполнение объявляя цикл до значения переменной length
+    {                                                           // тело цикла
+        Console.WriteLine(text[index]);       //заполняем аргумент text случайными числами
+        //index = index + 1;                                //увеличиваем счетчик на 1
+        index++;
+    }                                                           //тело цикла
+}
+
+void WaitUser() //метод (процедура) ожидание пользователя
+{
+    Console.WriteLine("Нажмите ENTER, когда будете готовы продолжить");
+
+    while (true)
+    {
+        if (Convert.ToString(Console.ReadLine()) == "")
+        {
+            break;
+        }
+
+    }
+}
+
+int SizeOfArray() //Метод ожидающий ввода числа элементов массиваи осуществляющий проверку ввода
+{
+    int Size = 0;
+    while (true)
+    {
+        Console.WriteLine("Введите число элементов массива");
+        if (int.TryParse((Console.ReadLine()), out int number))
+        {
+            Size = number;
+            Console.Clear();
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+        }
+    }
+    return Size;
+}
+
+string[] array = {"Задача", "Cформировать случайным образом целочисленный массив A",
+              "из натуральных двузначных чисел.", "Создать на его основе масив B, отбрасывая те, которые:",
+              "1  нарушают порядок возрастания", "2  больше среднего арифметического элементов A",
+              "3  чётные"};
+
+PrintTask(array);
+
+WaitUser();
+
+int size = SizeOfArray();
+int[] A = new int[size];
+
+FillArray(A);
+Console.WriteLine("Элементы массива A:");
+
+PrintArray(A);
+Console.WriteLine();
+
+int sizeB = RightOrderSize(A);
+
+int[] B = new int[Convert.ToInt32(sizeB) + 1];
+
+RightOrderFill(A, B);
+
+Console.WriteLine("Элементы массива B:");
+Console.WriteLine();
+
+PrintArray(B);
+Console.WriteLine();
 
 
 
