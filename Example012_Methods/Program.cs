2aefd08b245b42875723c756affc8c3119f3c656
AbubakirovRA <AbubakirovRA@ya.ru>
2021-11-07 16:23:20 +0500

    ass sort methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -107,91 +107,115 @@
 //ВЛОЖЕННЫЙ ЦИКЛ ДЛЯ ВЫВОДА ТАБЛИЦЫ УМНОЖЕНИЯ
 //-------------------------------------------
 
-/*README
-Данная программа забирает введеные значение в виде баллов поочередно за каждый отвеченный вопрос.
-ВНИМАНИЕ! ЕСЛИ ЗНАЧЕНИЯ БУДУТ ВВЕДЕНЫ ОТЛИЧНЫЕ ОТ ЦИФР, ПРОГРАММА ЗАКРОЕТСЯ, БУДЬТЕ ВНИМАТЕЛЬНЫ!
-После последнего вопроса она сравнивает все значения со шаблонными и суммирует не превосходящие:
-Если вы ответили 3, а в шаблоне 5, она суммирует 3;
-Если вы ответили 5, а в шаблоне 3, она суммирует 3.
-Затем считает процент сходства с шаблоном: Сумма ваших баллов / Сумму шаблонных.
-В конце выдает результат для каждой профессии и введенные вами значения.
-*/
+// void MultiplicationTable()
+// {
+//     for (int i = 2; i <= 10; i++)
+//     {
+//         for (int j = 2; j <= 10; j++)
+//         {
+//             Console.WriteLine($"{i} x {j} = {i * j}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-// Массив вопросов
-string[] NameQuetion =
+// MultiplicationTable ();
+
+// // // Дан текст. В тексте нужно все пробелы заменить черточками, 
+// // // маленькие буквы “к” заменить большими “К”,
+// // // a большие “С” заменить маленькими “с”.
+// // // Ясна ли задача?
+
+// string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие прусского короля. "
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string ReplaceText(string text, char oldValue, char newValue) //Тип данных char — это целочисленный тип данных, 
+//                                                               // который используется для представления символов. 
+//                                                               // То есть, каждому символу соответствует определённое 
+//                                                               // число из диапазона [0; 255]. Тип данных char также ещё 
+//                                                               // называют символьным типом данных, так как графическое 
+//                                                               // представление символов в С++ возможно благодаря char.
+// {
+//     string result = string.Empty;
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + text[i];
+//     }
+//     return result;
+// }
+
+// string newText=ReplaceText(text, ' ', '|');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+// newText=ReplaceText(text, 'к', 'К');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+//ФУНКЦИИ В ПРОГРАММИРОВАНИИ
+
+// СОРТИРОВКА ВЫБОРОМ
+//1. Найти позицию минимального элемента
+// в неотсортированной части массива
+// 2. Произвести обмен этого значения со
+// значением первой неотсортированной позиции
+// 3. Повторять пока есть неотсортированные элементы
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
 {
-    "Экстраверт",
-    "Интроверт",
-    "Способность к монотонной работе",
-    "Наставничество",
-    "Аналитические навыки",
-    "Эмпатия",
-    "Любопытство",
-    "Ораторское искусство",
-    "Организационные способности",
-    "Критическое мышление",
-    "Многозадачность",
-    "Креативность",
-    "Стрессоустойчивость",
-    "Контроль времени",
-    "Работа с большим объемом информации"
-};
-// Массивы каждой профессии int[] Example = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-int[] Programmer = { 2, 3, 4, 0, 4, 2, 5, 0, 1, 3, 3, 1, 4, 3, 5 };
-int[] Tester = { 2, 2, 5, 0, 3, 3, 4, 0, 1, 3, 3, 1, 2, 3, 3 };
-int[] Analytic = { 3, 4, 3, 0, 5, 2, 4, 1, 2, 5, 3, 3, 3, 2, 4 };
-int[] ProjectManager = { 4, 1, 1, 3, 3, 4, 3, 4, 5, 3, 5, 3, 5, 5, 4 };
-int[] ProductManager = { 3, 2, 3, 3, 3, 4, 2, 4, 4, 3, 5, 2, 4, 4, 3 };
-
-
-int count = NameQuetion.Length;
-int[] YourArray = new int[count];
-// Выделяем количество чисел из массива, повторяем операции, пока не достигнем конца массива
-for (int i = 0; i < count; i++)
-{
-    // Спрашиваем вопрос
-    Console.WriteLine($"Введите баллы с компетенции '{NameQuetion[i]}'");
-    // Вводим и заполняем массив с ответом
-    YourArray[i] = Convert.ToInt32(Console.ReadLine());
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
 
+Console.Write("Задан массив ");
+PrintArray(arr);
 
-// Подсчет суммы результата относительно введенных значений у каждой профессии
-void Result(int[] Array, int[] DefaultArray, string Profession)
+void SelectionSort(int[] array)
 {
-    // Находим сумму шаблонного массива
-    double SummDefault = 0;
-    for (int i = 0; i < DefaultArray.Length; i++)
+    for (int i = 0; i < array.Length - 1; i++)
     {
-        SummDefault += DefaultArray[i];
-    }
-    // Создаем цикл сравнения нашего балла с баллом шаблона
-    double Summ = 0;
-    for (int i = 0; i < Array.Length; i++)
-    {
-        // Если наш балл больше или равен шаблону, то записывается шаблон (макс кол баллов = баллы шаблона, больше не может быть)
-        // Если нет, то наш балл суммируется
-        if (Array[i] >= DefaultArray[i])
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
         {
-            Summ += DefaultArray[i];
+            if (array[j] < array[minPosition]) minPosition = j;
         }
-        else
-        {
-            Summ += Array[i];
-        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
     }
-    // Процентное соотношение каждой профессии относительно с введенным результатом
-    double SummRatio = (Summ / SummDefault) * 100;
-    // Консоль пишет полученый результат. Н: Вы "в профессии" на 33,33% (10 из 30)
-    Console.WriteLine($"Вы {Profession} на {SummRatio:F2}% ({Summ} из {SummDefault})");
 }
-Console.WriteLine();
-// Выводим введенные значения, чтобы скопировать
-for (int i = 0; i < YourArray.Length; i++) Console.WriteLine(NameQuetion[i] + " " + YourArray[i]);
-Console.WriteLine();
-// Считаем для каждой профессии
-Result(YourArray, Programmer, "Программист");
-Result(YourArray, Tester, "Тестировщик");
-Result(YourArray, Analytic, "Аналитик");
-Result(YourArray, ProjectManager, "Проджект-Менеджер");
-Result(YourArray, ProductManager, "Продукт-Менеджер");
+
+void SelectionSortMax(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+SelectionSort(arr);
+Console.Write("Сортировка с увеличением  ");
+PrintArray(arr);
+
+SelectionSortMax(arr);
+Console.Write("Сортировка с уменьшением  ");
+PrintArray(arr);
